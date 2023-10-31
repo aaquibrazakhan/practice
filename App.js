b4e400d1bd4343f0bc4676754f11567c3b06f559
aaquibrazakhan <120468114+aaquibrazakhan@users.noreply.github.com>
2023-11-01 02:23:36 +0530

    first practice

---
+++ App.js
@@ -0,0 +1,18 @@
+// const heading = React.createElement("h1", {}, "hello World from React!");
+
+//       const root = ReactDOM.createRoot(document.getElementById("root"));
+
+//       root.render(heading);
+
+
+
+const heading = React.createElement("h1", {}, "Hello World React!");
+
+const root= ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading)
+
+
+
+
+
